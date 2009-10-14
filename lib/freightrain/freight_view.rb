50fb0871a4c362ec04749114d601c46dedc4bc74
andrea@VEGETA <andrea@VEGETA.(none)>
2009-10-14 16:31:27 +0200

    freightrain module

--- lib/freightrain/freight_view.rb
+++ lib/freightrain/freight_view.rb
@@ -1,8 +1,13 @@
 
-class FreightView
-  include GtkBuilderHelper
-  
-  def initialize
-    load_from_file(File.join(File.dirname(__FILE__), "#{self.class.name}.glade"))
+module Freightrain
+
+  class FreightView
+    include GtkBuilderHelper
+
+    def initialize
+      load_from_file(File.join(File.dirname(__FILE__), "#{self.class.name}.glade"))
+    end
+
   end
+
 end
