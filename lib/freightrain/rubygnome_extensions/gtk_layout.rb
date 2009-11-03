7c92ca5f84ab884646110642c46f87444cd4495b
andrea@VEGETA <andrea@VEGETA.(none)>
2009-11-03 18:00:18 +0100

    layout stub

--- lib/freightrain/rubygnome_extensions/gtk_layout.rb
+++ lib/freightrain/rubygnome_extensions/gtk_layout.rb
@@ -3,12 +3,28 @@ module Freightrain
 
   class ElementsConverter
 
-    def initialize(layout)
-      @layout = layout
+    def initialize(layout, element_class, signals)
+      @layout             = layout
+      @element_class      = element_class
+      @signals            = signals
     end
 
     def from(enumerable)
-      
+      @layout.children.each do |child|
+        @layout.remove(child)
+      end
+      height = 0
+      enumerable.each do |value|
+        ui_element = @element_class.new
+        ui_element.value = value
+        ui_element.select if value == layout.selected_item
+        ui_element.signals.each do |key,signal|
+          signal.connect(@signals[key]) if @signals[key]
+        end
+        @layout.put(ui_element.control, 0, height * ui_element.control.height_request)
+        height += 1
+      end
+      @layout.height = @layout.elements.first.height_request if @layout.elements.first
     end
 
   end
@@ -18,13 +34,13 @@ module Gtk
 
   class Layout
 
-    def elements
-
-    end
-
     def bind(options)
       if options[:property] == :elements
-        options[:converter] ||= ElementsConverter.new(self, Freightrain["#{options[:control].to_s}View".to_convention_sym])
+        options[:converter] ||= ElementsConverter.new(
+          self,
+          Freightrain["#{options[:control].to_s}View".to_convention_sym],
+          options[:signals]
+        )
       end
     end
 
