33e91189f4e15289445452dae30f924e3bbd57fc
andrea@VEGETA <andrea@VEGETA.(none)>
2009-11-05 10:53:53 +0100

    gtk builder fix

--- lib/freightrain/gtk_builder_helper.rb
+++ lib/freightrain/gtk_builder_helper.rb
@@ -7,8 +7,10 @@ module Freightrain
       if File.exists?(file_path)
         builder.add_from_file(file_path)
         builder.objects.each do |widget|
-          method_name = widget.name
-          instance_eval "def #{method_name}; return @builder.get_object('#{method_name}');end"
+          if widget.respond_to? :name
+            method_name = widget.name
+            instance_eval "def #{method_name}; return @builder.get_object('#{method_name}');end"
+          end
         end
         builder.connect_signals do |handler|
           if self.respond_to? handler
