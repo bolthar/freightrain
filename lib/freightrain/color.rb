94a32958de6728298e22d68ad96dff68d1f8899c
andrea@VEGETA <andrea@VEGETA.(none)>
2009-11-06 13:21:15 +0100

    fix color

--- lib/freightrain/color.rb
+++ lib/freightrain/color.rb
@@ -6,9 +6,9 @@ module Freightrain
     def Color.rgb(rgb)
       value = rgb.to_s
       begin
-        red = value[0..1].hex * 256
-        green = value[2..3].hex * 256
-        blue = value[4..5].hex * 256
+        red   = (value[0..1].hex * 256) - 1
+        green = (value[2..3].hex * 256) - 1
+        blue  = (value[4..5].hex * 256) - 1
         return Gdk::Color.new(red,green,blue)
       rescue
         raise ArgumentError("Could not create a color from #{value}")
