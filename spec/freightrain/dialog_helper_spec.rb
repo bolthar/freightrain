25ed35d2db8b09831055ee25d9fd0110dbc73b9b
andrea@SONGOKU <andrea@SONGOKU.(none)>
2009-11-07 19:11:19 +0100

    DialogHelper specs

--- spec/freightrain/dialog_helper_spec.rb
+++ spec/freightrain/dialog_helper_spec.rb
@@ -14,6 +14,13 @@ describe DialogHelper do
       @extender.class.class_eval("include DialogHelper")
     end
 
+    it "should raise if extender does not respond to toplevel" do
+      @extender.stubs(:respond_to?).with(:toplevel).returns(false)
+      lambda do
+        @extender.open_file_dialog
+      end.should raise_error
+    end
+
     it "should call Gtk::FileChooserDialog with description provided" do
       param = "test description"
       Gtk::FileChooserDialog.expects(:new).with(
@@ -53,13 +60,91 @@ describe DialogHelper do
           :destroy => nil))
       @extender.open_file_dialog.should == result
     end
+  
+  end
+
+
+  describe "yes/no dialog" do
+
+    before :each do
+      Gtk::MessageDialog.stubs(:new).returns(stub_everything)
+      @extender = stub()
+      @extender.stubs(:respond_to?).with(:toplevel).returns(true)
+      @extender.stubs(:toplevel)
+      @extender.class.class_eval("include DialogHelper")
+    end
 
     it "should raise if extender does not respond to toplevel" do
       @extender.stubs(:respond_to?).with(:toplevel).returns(false)
-      lambda do        
-        @extender.open_file_dialog
+      lambda do
+        @extender.yes_no_dialog
       end.should raise_error
     end
+
+    it "should call Gtk::MessageDialog with description provided" do
+      param = "test description"
+      Gtk::MessageDialog.expects(:new).with(
+        anything,
+        anything,
+        anything,
+        anything,
+        param).returns(stub_everything)
+      @extender.yes_no_dialog(param)
+    end
+
+    it "should call title on dialog if title provided" do
+      test_title = "my title"
+      dialog = stub_everything()
+      Gtk::MessageDialog.stubs(:new).with(
+        anything,
+        anything,
+        anything,
+        anything,
+        anything).returns(dialog)
+      dialog.expects(:title=).with(test_title)
+      @extender.yes_no_dialog("dummy", test_title)
+    end
+
+    it "should call title on dialog with 'Warning!' if title not provided" do
+      dialog = stub_everything()
+      Gtk::MessageDialog.stubs(:new).with(
+        anything,
+        anything,
+        anything,
+        anything,
+        anything).returns(dialog)
+      dialog.expects(:title=).with("Warning!")
+      @extender.yes_no_dialog("dummy")
+    end
+
+    it "should call secondary_text on dialog if secondary text provided" do
+      test_secondary_text = "my secondary text"
+      dialog = stub_everything()
+      Gtk::MessageDialog.stubs(:new).with(
+        anything,
+        anything,
+        anything,
+        anything,
+        anything).returns(dialog)
+      dialog.expects(:secondary_text=).with(test_secondary_text)
+      @extender.yes_no_dialog("dummy", "dummy", test_secondary_text)
+    end
+
+    it "should call secondary_text on dialog with correct text if secondary text not provided" do
+      dialog = stub_everything()
+      Gtk::MessageDialog.stubs(:new).with(
+        anything,
+        anything,
+        anything,
+        anything,
+        anything).returns(dialog)
+      dialog.expects(:secondary_text=).with("It will not be possible to recover the deleted item")
+      @extender.yes_no_dialog("dummy")
+    end
+
+    it "should return false if user does not click yes"
+    it "should return true if user clicks yes"
+
   end
 end
 
