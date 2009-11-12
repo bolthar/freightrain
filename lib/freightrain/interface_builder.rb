13ef32adf06058c1a2e648d8d7f9e5db570d58ad
andrea@VEGETA <andrea@VEGETA.(none)>
2009-11-12 13:47:57 +0100

    cleanup

--- lib/freightrain/interface_builder.rb
+++ lib/freightrain/interface_builder.rb
@@ -2,22 +2,6 @@
 module Freightrain
   module InterfaceBuilder
 
-#    def load_from_file(file_path,builder = GtkInterfaceBuilder.new)
-#      definition_file_path = file_path + "." + builder.extension
-#      if File.exists?(definition_file_path)
-#        builder.add_from_file(definition_file_path)
-#        builder.objects.each do |widget|
-#          method_name = builder.get_widget_name(widget)
-#          instance_eval "def #{method_name}; return @builder.get_object('#{method_name}');end;"
-#        end
-#        builder.connect_signals do |handler|
-#          if self.respond_to? handler
-#            method(handler)
-#          end
-#        end
-#      end
-#    end
-
     def load_from_file(file_path,builder = GtkInterfaceBuilder.new)
       if builder.file_found?(file_path)
         @widgets = builder.create_objects_from_file(file_path)
