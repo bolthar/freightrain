33cc314d22af127a9e31d45e28fb0d9571d9663c
andrea@VEGETA <andrea@VEGETA.(none)>
2009-11-13 09:06:27 +0100

    qt binding test

--- lib/freightrain/toolkit_extensions/qt/widgets/qt_base.rb
+++ lib/freightrain/toolkit_extensions/qt/widgets/qt_base.rb
@@ -3,11 +3,19 @@ module Qt
 
   class Base
 
-    def bindings
-      return []
+    def bind(binding_options)
+      @bindings ||= {}
+      binding = FreightBinding.new(self, binding_options)
+      #TODO: send out warning on override?
+      @bindings[binding_options[:property]] = binding
+      return binding
     end
 
-
+    def bindings
+      @bindings ||= {}
+      return @bindings.values
+    end
+    
   end
 
 end
