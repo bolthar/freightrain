8bcc0ddcecad4c599216f3c925c5cbb898ecfd16
andrea@VEGETA <andrea@VEGETA.(none)>
2009-12-26 19:57:37 +0100

    freightview spec fix

--- spec/freightrain/freight_view_spec.rb
+++ spec/freightrain/freight_view_spec.rb
@@ -6,6 +6,10 @@ describe FreightView do
   before :each do
     @class = Class.new(FreightView)
     File.stubs(:join).returns("nofile")
+    Freightrain::Toolkit = Module.new
+    Freightrain::Toolkit::InterfaceBuilder = Class.new
+    Freightrain::Toolkit::InterfaceBuilder.stubs(:new).returns(stub(:file_found? => false))
+    @class.stubs(:instance_eval)
   end
 
   after :each do 
@@ -15,10 +19,6 @@ describe FreightView do
 
   it "should always include BindingHost" do
     @class.included_modules.include?(BindingHost).should == true
-  end
-
-  it "should always include DialogHelper" do
-    @class.included_modules.include?(DialogHelper).should == true
   end
 
   it "should always include GtkBuilderHelper" do
@@ -33,32 +33,6 @@ describe FreightView do
     @class.kind_of?(ContainerHookable).should == true
   end
 
-  describe "ctor" do
-
-    before :each do
-      @class.send(:define_method, :called) do
-        return @called
-      end
-    end
-
-    it "should always call load_from_file with right params" do
-      @class.stubs(:name).returns("myname")
-      File.stubs(:join).with(anything, "views", "myname").returns(:correct)
-      @class.send(:define_method, :load_from_file) do |path, builder|
-        @called = true if (path == :correct && builder == :builder)
-      end
-      @class.new(:builder).called.should == true
-    end
-
-    it "should always call create_signals" do
-      @class.send(:define_method, :create_signals) do
-        @called = true
-      end
-      @class.new.called.should == true
-    end
-
-  end
-  
   describe "container options" do
 
     it "should always return an hash with model == prototype" do
@@ -66,37 +40,39 @@ describe FreightView do
     end
 
   end
+  
+  describe "ctor" do
 
-  describe "toplevel" do
+    before :each do
+      @class.stubs(:instance_eval).with("include Toolkit::DialogHelper")
+      
+    end
 
-    it "should always return first object's toplevel" do
-      builder = stub()
-      builder.stubs(:extension).returns("dmy")
-      builder.stubs(:toplevel).returns(:correct)
-      view = @class.new(builder)
-      view.toplevel.should == :correct
+    it "should include Toolkit::DialogHelper if toolkit defined" do
+      @class.stubs(:name).returns("myname")
+      @class.stubs(:instance_eval).with("include Toolkit::DialogHelper")
+      @class.new()
+    end
+
+    it "should call load_from_file with right params if toolkit defined" do
+      @class.stubs(:name).returns("myname")      
+      Freightrain::Toolkit::InterfaceBuilder.stubs(:new).returns(:builder)
+      @class.send(:define_method, :load_from_file) do |path, builder|
+        @called = true if (path == "myname" && builder == :builder)
+      end
+      @class.new().instance_variable_get(:@called).should == true
+    end
+
+    it "should always call create_signals" do
+      @class.send(:define_method, :create_signals) do
+        @called = true
+      end
+      @class.new.instance_variable_get(:@called).should == true
     end
 
   end
-
-  describe "widgets" do
-
-    it "should return all objects in builder" do
-      builder = stub()
-      builder.stubs(:extension).returns("dmy")
-      builder.stubs(:objects).returns([:a, :b, :c])
-      view = @class.new(builder)
-      view.widgets.length.should == 3
-    end
-
- end
-
   
-
- 
-
-
-
+  
 
 end
 
