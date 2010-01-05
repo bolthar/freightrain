7a79a17cd5ee7de6f15568665b12bad1947d746a
andrea@VEGETA <andrea@VEGETA.(none)>
2010-01-05 15:51:10 +0100

    color

--- lib/extensions/gtk/widgets/gtk_widget.rb
+++ lib/extensions/gtk/widgets/gtk_widget.rb
@@ -17,22 +17,22 @@ module Gtk
     end
 
     def color_from_rgb(string)
-      red   = (string[0..1].hex) * 256
-      green = (string[2..3].hex) * 256
-      blue  = (string[4..5].hex) * 256
+      red   = ((string[0..1].hex.to_i + 1) * 256) - 1
+      green = ((string[2..3].hex.to_i + 1) * 256) - 1
+      blue  = ((string[4..5].hex.to_i + 1) * 256) - 1
       return Gdk::Color.new(red, green, blue)
     end
 
     def background=(color)
-      self.modify_bg(Gtk::STATE_NORMAL,color_from_rgb(color))
+      self.modify_bg(Gtk::STATE_NORMAL, color_from_rgb(color))
     end
 
     def foreground=(color)
-      self.modify_fg(Gtk::STATE_NORMAL,color_from_rgb(color))
+      self.modify_fg(Gtk::STATE_NORMAL, color_from_rgb(color))
     end
 
     def base_background=(color)
-      self.modify_fg(Gtk::STATE_NORMAL,color_from_rgb(color))
+      self.modify_base(Gtk::STATE_NORMAL, color_from_rgb(color))
     end
 
     def plug_in(region_widget)
