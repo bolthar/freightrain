d7d6475e08167993580d5fcee2a93622608afac1
andrea@VEGETA <andrea@VEGETA.(none)>
2010-01-06 16:53:26 +0100

    gtk combo

---
+++ lib/extensions/gtk/widgets/gtk_combo.rb
@@ -0,0 +1,28 @@
+
+module Gtk
+
+  class Combo
+
+    def elements=(enumerable)
+      list_store = Gtk::ListStore.new(Object, String)
+      enumerable.each do |item|
+        iterator = list_store.append
+        iterator[0] = item
+        iterator[1] = item.send(@text)
+      end
+      self.model = list_store
+    end
+
+    def bind(options)
+      if options[:property].to_sym == :elements
+        @path = options[:path]
+        @text = options[:text] || "to_s"
+        renderer = Gtk::CellRendererText.new
+        self.pack_start(renderer, true)
+        self.add_attribute(renderer,:text, 1)
+      end
+      super(options)
+    end
+
+  end
+end
