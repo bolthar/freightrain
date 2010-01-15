014887ec288df262d77dd3e5abf633cb57d975c7
andrea@VEGETA <andrea@VEGETA.(none)>
2010-01-15 11:47:04 +0100

    scaffolding, uglyuglyugly

--- lib/scaffolding/commands/triad.rb
+++ lib/scaffolding/commands/triad.rb
@@ -10,6 +10,11 @@ class TriadCommand
   def check
     raise "not a freightrain application" unless File.exist?(File.join(@path, "views"))
     raise "not a freightrain application" unless File.exist?(File.join(@path, "viewmodels"))
+    raise "#{@name} view already exists"  if File.exist?(File.join(@path, "views", "#{@name}_view.rb"))
+    raise "#{@name} view definition already exists" if File.exist?(File.join(@path, "views", "#{@name}_view.glade"))
+    raise "#{@name} view binding file already exists" if File.exist?(File.join(@path, "views", "#{@name}_view.bnd.yml"))
+    raise "#{@name} viewmodel already exists" if File.exist?(File.join(@path, "viewmodels", "#{@name}_view_model.rb"))
+
   end
 
   def execute
@@ -17,6 +22,7 @@ class TriadCommand
     Generator.create_stub(File.join(@path, "views", "#{@name}_view.glade"), "view_control_glade",:name => @name)
     Generator.create_stub(File.join(@path, "views", "#{@name}_view.bnd.yml"), "view_bnd", :name => @name)
     Generator.create_stub(File.join(@path, "viewmodels", "#{@name}_view_model.rb"), "view_model", :name => @name.capitalize)
+    print "#{@name} triad created\n"
   end
 
 
