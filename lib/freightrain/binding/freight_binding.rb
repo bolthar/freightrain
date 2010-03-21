d85fa4d425a3bb757254c44ac156848898ddcbe5
andrea@VEGETA <andrea@VEGETA.(none)>
2010-03-21 15:22:47 +0100

    removed exception tracing in binding (for specs... dude fix this, it's getting annoying)

--- lib/freightrain/binding/freight_binding.rb
+++ lib/freightrain/binding/freight_binding.rb
@@ -22,9 +22,9 @@ module Freightrain
           @cache = value
         end
       rescue Exception => ex
-        p "#{@widget.name} - update"
-        p ex.message
-        p @path
+#        p "#{@widget.name} - update"
+#        p ex.message
+#        p @path
       end
     end
 
