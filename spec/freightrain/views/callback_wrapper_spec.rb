7959b58f18a5f29c7c7b3c550d33c6970960b7a5
andrea@VEGETA <andrea@VEGETA.(none)>
2010-03-26 16:30:46 +0100

    callback wrapper specced

--- spec/freightrain/views/callback_wrapper_spec.rb
+++ spec/freightrain/views/callback_wrapper_spec.rb
@@ -2,6 +2,47 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe CallbackWrapper do
 
-  it "should have some specs"
-  
+  describe "matches widget?" do
+
+    it "should return false if widget name is not contained in method name" do
+      method = stub(:name => "bau!!")
+      result = CallbackWrapper.new(method).matches_widget?(stub(:name => "my_name"))
+      result.should == false
+    end
+
+    it "should return true if widget name is after event name" do
+      method = stub(:name => "on_test_my_name")
+      result = CallbackWrapper.new(method).matches_widget?(stub(:name => "my_name"))
+      result.should == false
+    end
+
+    it "should return true if widget name is before on" do
+      method = stub(:name => "my_name_on_test")
+      result = CallbackWrapper.new(method).matches_widget?(stub(:name => "my_name"))
+      result.should == true
+    end
+
+    it "should return true if widget name is after on" do
+      method = stub(:name => "on_my_name_test")
+      result = CallbackWrapper.new(method).matches_widget?(stub(:name => "my_name"))
+      result.should == true
+    end
+
+  end
+
+  describe "event name" do
+
+    it "should return tokens after widget name" do
+      method = stub(:name => "on_my_name_test")
+      result = CallbackWrapper.new(method).event_name(stub(:name => "my_name"))
+      result.should == "test"
+    end
+
+    it "should replace _ with - " do
+      method = stub(:name => "on_my_name_test_event")
+      result = CallbackWrapper.new(method).event_name(stub(:name => "my_name"))
+      result.should == "test-event"
+    end
+  end
+
 end
