359f12908eb574f150ce70092d4daa659207293c
andrea@VEGETA <andrea@VEGETA.(none)>
2010-04-07 11:43:11 +0200

    autostubber skeleton

--- lib/freightrain/testhelpers/auto_stubber.rb
+++ lib/freightrain/testhelpers/auto_stubber.rb
@@ -5,8 +5,16 @@ module Freightrain
 
     def self.stub_viewmodel(name)
       viewmodel_name = "#{name}_view_model"
-      Object.const_get(viewmodel_name).define_method(:bootstrap) { }
+      viewmodel_class = Object.const_get(viewmodel_name.from_convention)
+      viewmodel_class.send(:define_method, :bootstrap) { }
       viewmodel = Freightrain[viewmodel_name.to_sym]
+      viewmodel_class.instance_variable_get(:@services).each do |service|
+        #stub service (recursively!)
+          #stub each public method
+          #inject it somewhere (on viewmodel?)
+      end
+
+      return viewmodel
     end
 
   end
