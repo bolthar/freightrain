3fe503e57665924dce31a5536022a6118bc115d6
andrea@VEGETA <andrea@VEGETA.(none)>
2010-04-09 11:33:53 +0200

    autostubber refactor

--- lib/freightrain/testhelpers/auto_stubber.rb
+++ lib/freightrain/testhelpers/auto_stubber.rb
@@ -6,75 +6,73 @@ module Freightrain
   class AutoStubber
     include Mocha::API
 
-    def stub_viewmodel(name)
-      viewmodel_name = "#{name}_view_model"
-      viewmodel_class = Object.const_get(viewmodel_name.from_convention)
-      viewmodel_class.send(:define_method, :bootstrap) { }
-      viewmodel = Freightrain[viewmodel_name.to_sym]
-      #SERVICES
-      viewmodel_class.instance_variable_get(:@services).each do |service|
-        service_stub = stub_everything
-        viewmodel.instance_variable_set("@#{service}", service_stub)
-        viewmodel.class.send(:define_method, service) do
-          return instance_variable_get("@#{service}")
-        end
-      end
-      viewmodel.instance_variable_set(:@view, stub_everything)
-      viewmodel.class.send(:define_method, "view") do
-        return instance_variable_get(:@view)
-      end
-      #SIGNALS
+    #semi-private API
+    def stub_signals(signal_host)
       stub_signals = {}
       signal_names = []
-      signals      = viewmodel_class.instance_variable_get(:@signals)
+      signals      = signal_host.class.instance_variable_get(:@signals)
       signal_names = signals.keys if signals
       signal_names.each do |signal_name|
         stub_signals[signal_name] = stub_everything
       end
-      viewmodel.instance_variable_set(:@signals, stub_signals)
-      viewmodel_class.send(:define_method, :should_fire) do |signal_name|
+      signal_host.instance_variable_set(:@signals, stub_signals)
+      signal_host.class.send(:define_method, :should_fire) do |signal_name|
         return instance_variable_get(:@signals)[signal_name].expects(:fire)
       end
-      #REGIONS
+    end
+
+    def stub_regions(region_host)
       stub_regions = {}
       region_names = []
-      regions      = viewmodel_class.instance_variable_get(:@regions)
+      regions      = region_host.class.instance_variable_get(:@regions)
       region_names = regions.keys if regions
       region_names.each do |region_name|
         stub_regions[region_name] = stub_everything
-        viewmodel.class.send(:define_method, region_name) do
+        region_host.class.send(:define_method, region_name) do
           return instance_variable_get(:@regions)[region_name]
         end
       end
-      viewmodel.instance_variable_set(:@regions, stub_regions)
-      viewmodel_class.send(:define_method, :should_change_region) do |region_name, viewmodel_name|
-        return viewmodel.expects(:change_region).with(region_name, viewmodel_name)
+      region_host.instance_variable_set(:@regions, stub_regions)
+      region_host.send(:define_method, :should_change_region) do |region_name, viewmodel_name|
+        return region_host.expects(:change_region).with(region_name, viewmodel_name)
       end
+    end
+
+    def stub_services(service_host)
+      service_host.class.instance_variable_get(:@services).each do |service|
+        service_stub = stub_everything
+        service_host.instance_variable_set("@#{service}", service_stub)
+        service_host.class.send(:define_method, service) do
+          return instance_variable_get("@#{service}")
+        end
+      end
+    end
+
+    def stub_view(viewmodel)
+      viewmodel.instance_variable_set(:@view, stub_everything)
+      viewmodel.class.send(:define_method, "view") do
+        return instance_variable_get(:@view)
+      end
+    end
+
+    def stub_viewmodel(name)
+      viewmodel_name = "#{name}_view_model"
+      viewmodel = Freightrain[viewmodel_name.to_sym]
+      viewmodel.class.send(:define_method, :bootstrap) { }
+      stub_services(viewmodel)
+      stub_signals(viewmodel)
+      stub_regions(viewmodel)
+      stub_view(viewmodel)
       return viewmodel
     end
     
     def stub_service(name)
       service_name  = "#{name}_service"
-      service_class = Object.const_get(service_name.from_convention)
       service       = Freightrain[service_name.to_sym]
-      service_class.instance_variable_get(:@services).each do |subservice|
-        service_stub = stub_everything
-        service.instance_variable_set("@#{subservice}", service_stub)
-        service.class.send(:define_method, subservice) do
-          return instance_variable_get("@#{subservice}")
-        end
-      end
-      stub_signals = {}
-      service_class.instance_variable_get(:@signals).keys.each do |signal_name|
-        stub_signals[signal_name] = stub_everything
-      end
-      service.instance_variable_set(:@signals, stub_signals)
-      service_class.send(:define_method, :should_fire) do |signal_name|
-        return instance_variable_get(:@signals)[signal_name].expects(:fire)
-      end
+      stub_services(service)
+      stub_signals(service)
       return service
     end
-
     
   end
 end
