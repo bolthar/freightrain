44907c78cf2cc4fc30ab25458c6a191bda26a333
andrea@VEGETA <andrea@VEGETA.(none)>
2010-04-09 09:59:00 +0200

    autostubber test, no regions atm

--- lib/freightrain/testhelpers.rb
+++ lib/freightrain/testhelpers.rb
@@ -1,3 +1,2 @@
 
-
 require_all File.join(File.dirname(__FILE__), 'testhelpers')
--- lib/freightrain/testhelpers/auto_stubber.rb
+++ lib/freightrain/testhelpers/auto_stubber.rb
@@ -1,21 +1,29 @@
 
+require 'mocha/api'
+
 module Freightrain
 
-  module AutoStubber
+  class AutoStubber
+    include Mocha::API
 
-    def self.stub_viewmodel(name)
+    def stub_viewmodel(name)
       viewmodel_name = "#{name}_view_model"
       viewmodel_class = Object.const_get(viewmodel_name.from_convention)
       viewmodel_class.send(:define_method, :bootstrap) { }
       viewmodel = Freightrain[viewmodel_name.to_sym]
       viewmodel_class.instance_variable_get(:@services).each do |service|
-        #stub service (recursively!)
-          #stub each public method
-          #inject it somewhere (on viewmodel?)
+        service_stub = stub_everything
+        viewmodel.instance_variable_set("@#{service}", service_stub)
+        viewmodel.class.send(:define_method, service) do
+          return instance_variable_get("@#{service}")
+        end
       end
-
+      viewmodel.instance_variable_set(:@view, stub_everything)
+      viewmodel.class.send(:define_method, "view") do
+        return instance_variable_get(:@view)
+      end
       return viewmodel
     end
-
+    
   end
 end
