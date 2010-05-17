3a0f973b279f957c5c2c2148106cfa3c3b90c864
andrea@VEGETA <andrea@VEGETA.(none)>
2010-05-17 23:01:18 +0200

    qt support - layout widget visualization (no signals hooking)

--- lib/extensions/qt/widgets/qt_widget.rb
+++ lib/extensions/qt/widgets/qt_widget.rb
@@ -1,19 +1,30 @@
 
-
 module Qt
 
   class Widget
+    include Freightrain::LayoutWidget
+    
+    def setup
+      @signals ||= {}
+      selected_callback   = @signals[:selected]
+      @signals[:selected] = lambda do |value|
+                              elements.each do |item|
+                                item.set_selection(item.value == value)
+                              end
+                              selected_callback.call(value) if selected_callback
+                            end
+      @elements ||= []
+      @ready = true
+    end
 
     def elements=(enumerable)
+      setup unless @ready
       delta = enumerable.length - @elements.length
-      @height_factor = @control.new.control.height unless @height_factor
+      @height_factor = self.get_new_item.control.height unless @height_factor
       height = @elements.length
       delta.abs.times do
         if delta > 0
-          item = @control.new
-          item.signals.each do |key, signal|
-            signal.connect(@signals[key]) if @signals[key]
-          end
+          item = self.get_new_item
           item.control.geometry = Qt::Rect.new(
             0,
             item.control.height * height,
@@ -42,19 +53,29 @@ module Qt
       return @elements
     end
 
+    def selected
+      return @elements.select { |element| element.selected == true}.first
+    end
+
+    def selected=(val)
+      @elements.each do |element|
+        element.set_selection(val == element.value)
+      end
+    end
+
+    def get_new_item
+      item = Freightrain[@viewmodel]
+      item.signals.each do |key, signal|
+        signal.connect(@signals[key]) if @signals[key]
+      end
+      return item
+    end
+
     def bind(options)
-      if options[:property] == :elements
-        @elements           = []
-        @control            = options[:control]
+      if options[:property].to_sym == :elements
+        @viewmodel          = (options[:element].to_s + "_element_view_model").to_sym
         @signals            = options[:signals]
         options[:force]     = true
-        selected_callback   = @signals[:selected]
-        @signals[:selected] = lambda do |value|
-                                elements.each do |item|
-                                  item.set_ui_selection(item.value == value)
-                                end
-                                selected_callback.call(value) if selected_callback
-                              end
       end
       super(options)
     end
