91f689d7119528aac562d0fb3de49e40b2fcbddc
andrea <andrea@SONGOKU.(none)>
2012-06-08 11:00:32 +0200

    removed empty .bnd file from scaffolding

--- lib/scaffolding/commands/app.rb
+++ lib/scaffolding/commands/app.rb
@@ -22,7 +22,6 @@ class AppCommand
     Generator.create_stub(File.join(@path, "#{@name}.rb"),"application", :name => @name)
     Generator.create_stub(File.join(@path, "lib", "views", "#{@name}_view.rb"), "view_toplevel", :name => @name.capitalize, :toplevel => @name)
     Generator.create_stub(File.join(@path, "lib", "views", "#{@name}_view.glade"), "view_toplevel_glade",:name => @name)
-    Generator.create_stub(File.join(@path, "lib", "views", "#{@name}_view.bnd.yml"), "view_bnd", :name => @name)
     Generator.create_stub(File.join(@path, "lib", "viewmodels", "#{@name}_view_model.rb"), "viewmodel_toplevel", :name => @name.capitalize)
     print "Application created in #{@path}\n"
   end
--- lib/scaffolding/commands/triad.rb
+++ lib/scaffolding/commands/triad.rb
@@ -20,7 +20,6 @@ class TriadCommand
   def execute
     Generator.create_stub(File.join(@path, "views", "#{@name}_view.rb"), "view", :name => @name.capitalize)
     Generator.create_stub(File.join(@path, "views", "#{@name}_view.glade"), "view_control_glade",:name => @name)
-    Generator.create_stub(File.join(@path, "views", "#{@name}_view.bnd.yml"), "view_bnd", :name => @name)
     Generator.create_stub(File.join(@path, "viewmodels", "#{@name}_view_model.rb"), "view_model", :name => @name.capitalize)
     print "#{@name} triad created\n"
   end
